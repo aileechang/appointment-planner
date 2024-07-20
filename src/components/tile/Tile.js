508514c868600804803a0cd0d98897ea7a695fcb
aileechang <aileechang.swe@gmail.com>
2024-07-19 23:20:28 -0800

    feat: Implement reusable Tile component

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ name, description }) => {
+  const descriptionValues = Object.values(description);
+
   return (
     <div className="tile-container">
-      
+      <p className="tile-title" >{name}</p>
+      {descriptionValues.map((value, index) => (
+        <p key={index} className="tile" >{value}</p>
+      ))}
     </div>
   );
 };
